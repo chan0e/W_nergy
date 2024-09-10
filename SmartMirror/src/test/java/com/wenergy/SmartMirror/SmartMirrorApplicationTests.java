24ea82511059788192bac920021161580f79e06d
정찬영 <94053008+chan0e@users.noreply.github.com>
2024-09-10 12:11:20 +0900

    Add files via upload
    project push

---
+++ SmartMirror/src/test/java/com/wenergy/SmartMirror/SmartMirrorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wenergy.SmartMirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMirrorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
